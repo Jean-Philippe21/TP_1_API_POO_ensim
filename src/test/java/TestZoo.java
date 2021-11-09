404bb657e8083ef4b74840bca55a08c467e8b3c5
Jean-Philippe21 <jeanphilippeavossevou@gmail.com>
2021-11-09 15:54:27 +0100

    Mise a jour 0

--- src/test/java/TestZoo.java
+++ src/test/java/TestZoo.java
@@ -4,7 +4,7 @@ public class TestZoo {
     @Test
     public void test(){
         Exception o = null;
-        Zoo monZoo = new Zoo(1,3);
+        Zoo monZoo = new Zoo(1,2);
 
         try {
             monZoo.setVisiteurs(3);
